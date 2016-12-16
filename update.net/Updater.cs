572c15272f7456c5ef7838012e66d8cadc26008e
Brandon Milton <brandonio21@linux.com>
2016-12-16 15:49:38 -0800

    Add test and Updater base class

---
+++ update.net/Updater.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace update.net
+{
+    /* An Updater implementation of the interface that contains some
+     * common functionality that may be utilized for all updaters */
+    public abstract class Updater : IUpdater
+    {
+        protected string versionURL;
+        protected string updaterURL;
+        protected string changelogURL;
+        protected string username;
+        protected string password;
+        protected string updaterPath;
+
+        public event EventHandler UpdateDownloaded;
+        public event EventHandler UpdateDownloadProgressChanged;
+
+        public Updater()
+        {
+            this.versionURL = String.Empty;
+            this.updaterURL = String.Empty;
+            this.changelogURL = String.Empty;
+            this.username = String.Empty;
+            this.password = String.Empty;
+        }
+
+        public Updater(
+            string versionURL,
+            string updaterURL,
+            string directory,
+            string changelogURL = null,
+            string username = null,
+            string password = null)
+        {
+            versionURL.AssertNotNullNotEmpty("versionURL");
+            updaterURL.AssertNotNullNotEmpty("updaterURL");
+            directory.AssertNotNullNotEmpty("directory");
+
+            this.versionURL = versionURL;
+            this.updaterURL = updaterURL;
+            this.changelogURL = changelogURL;
+            this.username = username == null ? String.Empty : username;
+            this.password = password == null ? String.Empty : password;
+
+            // Create the update directory if it doesn't exist already
+            System.IO.Directory.CreateDirectory(directory);
+
+            string updaterName = Path.GetFileName(updaterURL);
+            this.updaterPath = Path.Combine(directory, updaterName);
+        }
+
+        protected WebClient GetWebClient(
+            Encoding encoding = null)
+        {
+            Encoding webClientEncoding = encoding != null ? encoding : System.Text.Encoding.UTF8;
+            var webclient = new WebClient {
+                Encoding = webClientEncoding,
+            };
+            webclient.Credentials = new NetworkCredential(
+                this.username, this.password);
+
+            return webclient;
+        }
+
+        public bool IsUpdateAvailable(int currentVersion)
+        {
+            int latestVersion = this.GetLatestVersion();
+            return latestVersion > currentVersion;
+        }
+
+        protected void HandleDownloadUpdateComplete(
+            object sender,
+            System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            var handler = UpdateDownloaded;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected void HandleDownloadUpdateProgressChange(
+            object sender,
+            System.Net.DownloadProgressChangedEventArgs e)
+        {
+            var handler = UpdateDownloadProgressChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public void Clean()
+        {
+            if (File.Exists(this.updaterPath))
+            {
+                File.Delete(this.updaterPath);
+            }
+        }
+
+        public abstract int GetLatestVersion();
+        public abstract void DownloadUpdate();
+        public abstract void RunUpdate(string args = null);
+        public abstract string GetChangelog();
+    }
+}
---
+++ update.net_test/HTTPPlaintextUpdaterTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using update.net;
+using System.Threading;
+
+namespace update.net_test
+{
+    [TestClass]
+    public class HTTPPlaintextUpdaterTest
+    {
+        static string BIN_DIR = Directory.GetCurrentDirectory();
+        static string PROJ_DIR = Directory.GetParent(BIN_DIR).Parent.FullName;
+        static string SERVER_DIR = Path.Combine(PROJ_DIR, "TestServer");
+        static string LOCAL_DIR = Path.Combine(PROJ_DIR, "TestLocal");
+        static string UPDATER_URL = Path.Combine(SERVER_DIR, "updater.txt");
+        static string VERSION_URL = Path.Combine(SERVER_DIR, "version.txt");
+        static string CHANGELOG_URL = Path.Combine(SERVER_DIR, "changelog.txt");
+
+        private HTTPPlaintextUpdater updater;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.updater = new HTTPPlaintextUpdater(
+                VERSION_URL,
+                UPDATER_URL,
+                LOCAL_DIR,
+                CHANGELOG_URL);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+            "Providing a null version url did not throw")]
+        public void TestConstructorThrowsWhenNoVersionURL()
+        {
+            HTTPPlaintextUpdater updater = new HTTPPlaintextUpdater(
+                null,
+                "updater_url",
+                "directory");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+            "Providing a null updater url did not throw")]
+        public void TestConstructorThrowsWhenNoUpdaterURL()
+        {
+            HTTPPlaintextUpdater updater = new HTTPPlaintextUpdater(
+                "version_url",
+                null,
+                "directory");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+            "Providing a null directory did not throw")]
+        public void TestConstructorThrowsWhenNoDirectory()
+        {
+            HTTPPlaintextUpdater updater = new HTTPPlaintextUpdater(
+                "version_url",
+                "updater_url",
+                null);
+        }
+
+        [TestMethod]
+        public void TestGetLatestVersion()
+        {
+            int latestVersion = this.updater.GetLatestVersion();
+            Assert.AreEqual(50, latestVersion);
+        }
+
+        [TestMethod]
+        public void TestIsUpdateAvailable()
+        {
+            Assert.IsTrue(this.updater.IsUpdateAvailable(49));
+            Assert.IsFalse(this.updater.IsUpdateAvailable(51));
+        }
+
+        [TestMethod]
+        public void TestDownloadUpdate()
+        {
+            Semaphore updateDownloaded = new Semaphore(0, 1);
+            this.updater.UpdateDownloaded += (sender, e) =>
+            {
+                updateDownloaded.Release();
+            };
+
+            this.updater.DownloadUpdate();
+
+            Assert.IsTrue(updateDownloaded.WaitOne(TimeSpan.FromMilliseconds(500)),
+                "Update not downloaded succesfully");
+
+            string updatePath = Path.Combine(LOCAL_DIR, "updater.txt");
+            var updateReader = new StreamReader(updatePath);
+            Assert.AreEqual("update", updateReader.ReadToEnd());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetChangelogNoURL()
+        {
+            var updater = new HTTPPlaintextUpdater(
+                VERSION_URL,
+                UPDATER_URL,
+                SERVER_DIR);
+            updater.GetChangelog();
+        }
+
+        [TestMethod]
+        public void TestGetChangelog()
+        {
+            Assert.AreEqual("changelog", this.updater.GetChangelog());
+        }
+    }
+}
